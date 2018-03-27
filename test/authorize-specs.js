bb32d4ce7b0d5c0fb61bfdd6cec8a4551583fdb0
Dan Graham <dan@saucelabs.com>
2018-03-27 08:27:39 -0700

    No mocha arrows (#15)

--- test/authorize-specs.js
+++ test/authorize-specs.js
@@ -16,8 +16,8 @@ let SANDBOX = Symbol();
 let mocks = {};
 let libs = {teen_process, xcode, path};
 
-describe('Authorize test', () =>{
-  beforeEach(() => {
+describe('Authorize test', function (){
+  beforeEach(function () {
     sandbox = sinon.sandbox.create();
     mocks[SANDBOX] = sandbox;
     for (let [key, value] of _.pairs(libs)) {
@@ -25,17 +25,17 @@ describe('Authorize test', () =>{
     }
   });
 
-  afterEach(() => {
+  afterEach(function () {
     sandbox.restore();
   });
 
-  it('should throw an error', async () => {
+  it('should throw an error', async function () {
     mocks.teen_process.expects('exec').once().withExactArgs('DevToolsSecurity', ['--enable']).throws('Error');
     await authorize().should.eventually.be.rejectedWith('Error');
     mocks[SANDBOX].verify();
   });
 
-  it('should pass all mocks', async () => {
+  it('should pass all mocks', async function () {
     mocks.teen_process.expects('exec').atLeast(3);
 
     mocks.xcode.expects('getPath').once().returns('xcodeDir/Applications/Xcode.app/Contents/Developer');
